178176837735951fa4f3d51179c7263118cb89b5
Akinori-Ichigo <akinori.ichigo@gmail.com>
2023-11-25 12:16:39 +0900

    hrefアトリビュート内の文字エスケープを回避するようにした（&を&amp;にしたりしないように）。

--- lib/darkmouun/kramdown.rb
+++ lib/darkmouun/kramdown.rb
@@ -4,6 +4,7 @@ require_relative "kramdown/parser/kramdown/extensions"
 require_relative "kramdown/parser/kramdown/span"
 require_relative "kramdown/parser/kramdown/link"
 require_relative "kramdown/converter/html"
+require_relative 'kramdown/utils/html'
 
 module Kramdown
   class Element
